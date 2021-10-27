c984a8516bfde59008c1717119ad4e49e3188d4a
Calvin Remsburg <cremsburg@protonmail.com>
2021-10-27 00:13:22 +0000

    update

--- files/scrapli/app_async.py
+++ files/scrapli/app_async.py
@@ -1,6 +1,16 @@
 import asyncio
+import xmltodict
 
 from scrapli_netconf.driver import AsyncNetconfDriver
+from scrapli.logging import enable_basic_logging
+from jinja2 import Environment, FileSystemLoader
+
+# Enable logging. Create a log file in the current directory.
+enable_basic_logging(file=True, level="debug")
+
+# setup jinja2 parameters
+env = Environment(loader=FileSystemLoader('templates'),trim_blocks=True)
+template = env.get_template('test.j2')
 
 GALVESTON = {
     "host": "192.168.105.137",
@@ -40,9 +50,19 @@ async def main():
     coroutines = [gather_version(device) for device in DEVICES]
     results = await asyncio.gather(*coroutines)
     for each in results:
-        print(f"dir:\n{dir(each)}")
-        print(f"each:\n{each.result}")
+        reply_as_dict = xmltodict.parse(each.result)
+        security_zones = reply_as_dict["rpc-reply"]["zones-information"]["zones-security"]
 
+        output_from_parsed_template = template.render(security_zones=security_zones)
+        print(output_from_parsed_template)
+
+        # to save the results
+        with open(f"{each.host}.yaml", "w") as fh:
+            fh.write(output_from_parsed_template)
+
+        # print(f"{each.host}")
+        # for zone in security_zones:
+        #     print(f'{zone["zones-security-zonename"]}')
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
